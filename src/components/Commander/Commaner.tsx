1aaa666475f663d1e8486439bf2a200959d32f8e
Matt McGeown <matt_live@live.co.uk>
2023-03-23 13:46:14 +0000

    adding basic component

---
+++ src/components/Commander/Commaner.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+
+const Commander: FC<any> = () => {
+  return (
+    <div>
+      <h2>Commander</h2>
+    </div>
+  );
+};
+
+export default Commander;
+
+// Commander.defaultName('Commander')
---
+++ src/constants/Commands.ts
@@ -0,0 +1,14 @@
+export const COMMANDS = {
+  whois: [
+    {
+      cmd: 'whois',
+      output: 'I am me',
+    },
+  ],
+  whoami: [
+    {
+      cmd: 'whoami',
+      output: 'you are you',
+    },
+  ],
+};
